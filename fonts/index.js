5789ab7aac05d71a4b550a5962ca16a564848df6
Alammduran <alammduran@gmail.com>
2021-08-25 13:41:29 -0500

    Updates

--- fonts/index.js
+++
@@ -1,6 +0,0 @@
-export { default as RadikalBlack } from "../fonts/Radikal Black.otf";
-export { default as RadikalBold } from "../fonts/Radikal Bold.otf";
-export { default as RadikalLight } from "../fonts/Radikal Light.otf";
-export { default as RadikalMedium } from "../fonts/Radikal Medium.otf";
-export { default as Radikal } from "../fonts/Radikal.otf";
-export { default as RealBrush } from "../fonts/Real Brush.otf";
---
+++ precache-manifest.c693acf2e0e6caf769865a1aa48fc429.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad445e91e3b9ea6ab3972d02f72cba1",
+    "url": "/workpri/index.html"
+  },
+  {
+    "revision": "9253b88ae315c0ae1aa3",
+    "url": "/workpri/static/css/main.3eff5b18.chunk.css"
+  },
+  {
+    "revision": "249afff1ed399628577f",
+    "url": "/workpri/static/js/2.c91bb48e.chunk.js"
+  },
+  {
+    "revision": "9253b88ae315c0ae1aa3",
+    "url": "/workpri/static/js/main.bec81087.chunk.js"
+  },
+  {
+    "revision": "2316328b42cc90cd9e79",
+    "url": "/workpri/static/js/runtime-main.4a502946.js"
+  },
+  {
+    "revision": "39344647da9535d8376a17acc5c9465f",
+    "url": "/workpri/static/media/Radikal Bold.39344647.otf"
+  },
+  {
+    "revision": "659f0f8508c25870b3dfe10584cc8e71",
+    "url": "/workpri/static/media/Radikal Medium.659f0f85.otf"
+  },
+  {
+    "revision": "f39258e224200e30e4e0d5422111fbd8",
+    "url": "/workpri/static/media/Radikal.f39258e2.otf"
+  }
+]);
