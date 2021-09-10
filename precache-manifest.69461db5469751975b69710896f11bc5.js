f8da5fd07b8b5969eb493a7848d370ccfa5d57e8
validName <>
2021-09-09 21:12:58 -0500

    Updates

---
+++ precache-manifest.69461db5469751975b69710896f11bc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5244a43077154af080322d6bf23d38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cfa80e1fc4d6de3a7bf",
+    "url": "/static/css/main.c57b0b6d.chunk.css"
+  },
+  {
+    "revision": "5462c7f6c60ebc91827a",
+    "url": "/static/js/2.3712a46f.chunk.js"
+  },
+  {
+    "revision": "8cfa80e1fc4d6de3a7bf",
+    "url": "/static/js/main.e3699922.chunk.js"
+  },
+  {
+    "revision": "2bf98bf9cf9b666a9d94",
+    "url": "/static/js/runtime-main.fa6bcccc.js"
+  },
+  {
+    "revision": "39344647da9535d8376a17acc5c9465f",
+    "url": "/static/media/Radikal Bold.39344647.otf"
+  },
+  {
+    "revision": "659f0f8508c25870b3dfe10584cc8e71",
+    "url": "/static/media/Radikal Medium.659f0f85.otf"
+  },
+  {
+    "revision": "f39258e224200e30e4e0d5422111fbd8",
+    "url": "/static/media/Radikal.f39258e2.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/workpri/precache-manifest.8ef2cf1ba75798325a9c90752704fb95.js"
+  "/precache-manifest.69461db5469751975b69710896f11bc5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/workpri/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
